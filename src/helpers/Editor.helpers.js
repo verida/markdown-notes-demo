d50a5785863db6d3002ff4d43e62e552dbbe5a3a
Michael Chinweike <infinity_michael@yahoo.com>
2021-09-16 06:57:41 +0100

    fix: fixed text overflow

---
+++ src/helpers/Editor.helpers.js
@@ -0,0 +1,7 @@
+export const reduceStringLength = (text, length, startIndex = 0) => {
+  const words = text.split('');
+  if (words.length > length) {
+    return text.substring(startIndex, length);
+  }
+  return text;
+};
