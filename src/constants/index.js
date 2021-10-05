ef408f5130a989358749f481302fd4e476b3b3e1
Michael Chinweike <infinity_michael@yahoo.com>
2021-10-05 23:55:18 +0100

    feat:modified constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,16 +10,10 @@ export const LOGO_URL = 'http://assets.verida.io/verida_logo.svg';
 export const CLIENT_AUTH_NAME = 'Verida: Markdown Notes Demo';
 export const APP_NAME = 'Markdown notes';
 
-// TODO: Refactor to use .env to add schema urls
-
-// eslint-disable-next-line import/no-mutable-exports
-export let DATASTORE_SCHEMA;
-
-if (process.env.NODE_ENV === 'development') {
-  DATASTORE_SCHEMA = 'http://localhost:3008/schema.json';
-} else {
-  DATASTORE_SCHEMA = 'https://markdown-editor.demos.testnet.verida.io/schema.json';
-}
+export const DATASTORE_SCHEMA =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3008/schema.json'
+    : 'https://markdown-editor.demos.testnet.verida.io/schema.json';
 
 export const webLinks = {
   DOCUMENTATION: 'https://github.com/verida/markdown-notes-demo/blob/main/README.md',
