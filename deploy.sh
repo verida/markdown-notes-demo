a15d2107e0f04483a550cb981022f342aa40f2a7
Nick Lothian <nick@verida.io>
2023-01-18 14:31:46 +1030

    deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
-echo "Deployment is now done via AWS Amplify when commits are pushed to the main branch"
-echo "see https://us-east-2.console.aws.amazon.com/amplify/"
+# Do this EXACTLY. 
+# If you do "smart" things like adding messages to the merge, main branch ends up ahead of develop
+# This is because a comment forces a merge commmit, and we just want to fast-foward. 
+# See "MERGING TAG" under https://git-scm.com/docs/git-merge
+git checkout develop
+git pull
+git checkout main
+git pull
+git merge develop --ff-only 
+git push
